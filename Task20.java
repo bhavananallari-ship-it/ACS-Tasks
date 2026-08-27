48b1903b2c582959675254b1983102c2b528b9ff
bhavananallari-ship-it <bhavananallari@gmail.com>
2026-08-27 10:05:11 +0530

    Add files via upload

---
+++ Task20.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] answer = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+
+            // Find warmer temperature for previous days
+            while (!stack.isEmpty() &&
+                   temperatures[i] > temperatures[stack.peek()]) {
+
+                int prevIndex = stack.pop();
+                answer[prevIndex] = i - prevIndex;
+            }
+
+            // Store current index
+            stack.push(i);
+        }
+
+        return answer;
+    }
+}
---
+++ Task24.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder str = new StringBuilder(s);
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch == '(') {
+                stack.push(i);
+            } 
+            else if (ch == ')') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    str.setCharAt(i, '*');
+                }
+            }
+        }
+
+        // Remove unmatched '('
+        while (!stack.isEmpty()) {
+            str.setCharAt(stack.pop(), '*');
+        }
+
+        // Build final answer
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != '*') {
+                result.append(str.charAt(i));
+            }
+        }
+
+        return result.toString();
+    }
+}
